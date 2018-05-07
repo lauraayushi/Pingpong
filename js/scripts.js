55b7651f494085deaba5635d8973d93da48cd300
lauraayushi <royalsayushi@gmail.com>
2018-05-07 14:17:04 +0300

    add to page

--- js/scripts.js
+++ js/scripts.js
@@ -2,15 +2,11 @@
 var myNumber= function(countTo) {
 var result = [];
 for (var i=1;i<=countTo; i++) {
-	var index =result.indexOf(i);
 	if (i % 15 === 0) {
-	result.splice(index, 0);
 	result.push ('pingpong');
 	}else if (i % 5 === 0) {
-	 result.splice(index, 0);
 	 result.push ('pong');
 	}else if (i % 3 === 0) {
-	 result.splice(index, 0);
 	 result.push ('ping');
 	}else {
 	 result.push (i);
